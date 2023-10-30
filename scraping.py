da1410ae309156bd89a57bc5f3e562b537a6e441
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-10-29 22:47:20 -0700

    fixed adding folder

--- scraping.py
+++ scraping.py
@@ -3,13 +3,16 @@ from bs4 import BeautifulSoup
 import os
 
 # スクレイピングするURLを指定
-url ='https://books.toscrape.com/'
-# img_url = "media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg"
-# full_img_url = base_url + img_url
+
+base_url ='https://books.toscrape.com/'
+# category_url='catalogue/category/books/mystery_3/index.html'
+category_url='catalogue/category/books/sequential-art_5/index.html'
+
+full_url=base_url+category_url
 
 # requestsを使ってWebページを取得
-response = requests.get(url)
-
+# response = requests.get(base_url)
+response = requests.get(full_url)
 response.raise_for_status()
 
 # BeautifulSoupを使ってHTMLを解析
@@ -21,16 +24,24 @@ img_tags = soup.find_all('img')
 # 画像のURLを取得
 img_urls = [img['src'] for img in img_tags if 'src' in img.attrs]
 
+# URLからフォルダ名として使用する部分を取得
+folder_name = full_url.split('/')[-2]
+
+# フォルダが存在しない場合、フォルダを作成
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
 # 画像をダウンロードして保存
 for img_url in img_urls:
     # 相対URLを絶対URLに変換
-    full_img_url = os.path.join(url, img_url)
-    
+    # full_img_url = os.path.join(base_url, img_url)
+    full_img_url = base_url + img_url.lstrip('/')
+
     img_response = requests.get(full_img_url)
     img_response.raise_for_status()
 
     # ファイル名を生成
-    filename = os.path.join(".", os.path.basename(img_url))
+    filename = os.path.join(folder_name, os.path.basename(img_url))
     
     # 画像を保存
     with open(filename, 'wb') as f:
